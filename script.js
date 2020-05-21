c7ff81ac0dcdcf19cb1178df7f8310444dc2c654
unknown <xvinh.ir@gmail.com>
2020-05-21 15:00:42 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let tweetArea = document.getElementById("tweetArea")
+let MAX_LETTER = 140;
+const countLetter = () => {
+    let lengthOfSentence = tweetArea.value.length;
+    console.log("length is:", lengthOfSentence);
+    let remain = MAX_LETTER - lengthOfSentence;
+    document.getElementById("remain").innerHTML = `${remain} characters left`;
+    if (remain < 0) {
+        document.getElementById("remain").innerHTML = `Remain: ${remain}`;
+    }
+}
+tweetArea.addEventListener("input",countLetter)
